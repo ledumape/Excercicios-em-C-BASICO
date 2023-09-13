03f3e96df3afd10d8cff8a63abc2ddd0b703c2cb
ledumape <132487524+ledumape@users.noreply.github.com>
2023-09-13 11:49:52 -0300

    Update idade.cpp

--- idade.cpp
+++ idade.cpp
@@ -1,3 +1,5 @@
+/*Fazer um programa para ler o nome e idade de duas pessoas. Ao final mostrar uma mensagem com os 
+nomes e a idade média entre essas pessoas, com uma casa decimal*/
 #include <stdio.h>
 #include <string.h>
 int main(){
