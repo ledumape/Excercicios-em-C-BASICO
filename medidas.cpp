7b507bc0244220cc398516a382350026e49bb0b8
ledumape <132487524+ledumape@users.noreply.github.com>
2023-09-13 11:54:17 -0300

    Update medidas.cpp

--- medidas.cpp
+++ medidas.cpp
@@ -1,3 +1,10 @@
+/*Problema "medidas" 
+Fazer um programa para ler três medidas A, B e C. Em seguida, calcular e mostrar (imprimir os dados 
+com quatro casas decimais): 
+a) a área do quadrado que tem lado A 
+b) a área do triângulo retângulo que base A e altura B 
+c) a área do trapézio que tem bases A e B, e altura C */
+
 #include <stdio.h>
 int main(){
 	float A, B ,C , area, area2, area3;
