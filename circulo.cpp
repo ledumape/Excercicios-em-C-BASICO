264633248b01e5249d40182e27652797502385de
ledumape <132487524+ledumape@users.noreply.github.com>
2023-09-13 11:52:10 -0300

    Update circulo.cpp

--- circulo.cpp
+++ circulo.cpp
@@ -1,3 +1,9 @@
+/*Problema "circulo" 
+Fazer um programa para ler o valor "r" do raio de um círculo, e depois mostrar o valor da área do 
+círculo com três casas decimais. A fórmula da área do círculo é a seguinte: 𝑎𝑟𝑒𝑎 = 𝜋. 𝑟^2*/
+. Você pode 
+usar o valor de 𝜋 fornecido pela biblioteca da sua linguagem de programação
+
 #include <stdio.h>
 #include <math.h>
 
