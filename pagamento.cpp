3eddbaff9e8e4af38e6aba7253bcfe8def2387e8
ledumape <132487524+ledumape@users.noreply.github.com>
2023-09-13 11:52:48 -0300

    Update pagamento.cpp

--- pagamento.cpp
+++ pagamento.cpp
@@ -1,3 +1,7 @@
+/*Problema "pagamento" 
+Fazer um programa para ler o nome de um(a) funcionário(a), o valor que ele(a) recebe por hora, e a 
+quantidade de horas trabalhadas por ele(a). Ao final, mostrar o valor do pagamento do funcionário com 
+uma mensagem explicativa*/
 #include <stdio.h>
 int main(){
 	
