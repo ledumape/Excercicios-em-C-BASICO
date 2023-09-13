7f727b996325a10d979161fcda79de4b493320e5
ledumape <132487524+ledumape@users.noreply.github.com>
2023-09-13 11:53:52 -0300

    Update consumo.cpp

--- consumo.cpp
+++ consumo.cpp
@@ -1,3 +1,7 @@
+/*Problema "consumo" 
+Fazer um programa para ler a distância total (em km) percorrida por um carro, bem como o total de 
+combustível gasto por este carro ao percorrer tal distância. Seu programa deve mostrar o consumo 
+médio do carro, com três casas decimais. */
 #include <stdio.h>
 int main(){
 	float distancia, combustivel, media;
