e7ab832bfed6604a9cd7b09b3092a72d68295eb7
ledumape <132487524+ledumape@users.noreply.github.com>
2023-09-13 11:48:57 -0300

    Update retangulo.cpp

--- retangulo.cpp
+++ retangulo.cpp
@@ -1,3 +1,7 @@
+/*Problema "retangulo" 
+Fazer um programa para ler as medidas da base e altura de um retângulo. Em seguida, mostrar o valor 
+da área, perímetro e diagonal deste retângulo, com quatro casas decimais.*/ 
+
 #include <stdio.h>
 #include <math.h>
 int main(){
